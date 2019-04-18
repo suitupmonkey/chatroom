49cec2269fed9bd2272265af8d79faeb6147fcca
louwweiwei <louweiwei@sinosoft.com.cn>
2019-04-18 14:54:47 +0800

    聊天室背景颜色css，chatroom.js

---
+++ src/main/resources/static/js/app/chatroom/chatroom.js
@@ -0,0 +1,68 @@
+var socket,
+    msg,
+    receiveChannel,
+    chatWindow = document.querySelector('.chat-window'),
+    chatWindowMessage = document.querySelector('.chat-window-message'),
+    chatThread = document.querySelector('.chat-thread');
+
+if (!window.WebSocket) {
+    window.WebSocket = window.MozWebSocket;
+}
+
+if (window.WebSocket) {
+    socket = new WebSocket("ws://localhost:8082/websocket");
+    socket.onmessage = function (event) {
+
+        var chatNewThread = document.createElement('li'),
+            chatNewMessage = document.createTextNode(event.data);
+
+        // Add message to chat thread and scroll to bottom
+        chatNewThread.appendChild(chatNewMessage);
+        chatThread.appendChild(chatNewThread);
+        chatThread.scrollTop = chatThread.scrollHeight;
+
+        // Clear text value
+        chatWindowMessage.value = '';
+
+
+        // var ta = document.getElementById('responseText');
+        // ta.value = "";
+        // ta.value = event.data
+    };
+    socket.onopen = function (event) {
+
+    };
+    socket.onclose = function (event) {
+
+    };
+}
+else {
+    alert("抱歉，您的浏览器不支持WebSocket协议!");
+}
+
+function send(message) {
+    if (!window.WebSocket) {
+        return;
+    }
+    if (socket.readyState == WebSocket.OPEN) {
+        socket.send(message);
+    }
+    else {
+        alert("WebSocket连接没有建立成功!");
+    }
+}
+
+
+
+// On form submit, send message
+chatWindow.onsubmit = function (e) {
+    msg = $('#msg').val();
+    send(msg);
+    return false;
+};
+
+$("#chatWindow").keydown(function() {
+    if (event.keyCode == "13") {//keyCode=13是回车键
+        $('#msg').click();
+    }
+});
