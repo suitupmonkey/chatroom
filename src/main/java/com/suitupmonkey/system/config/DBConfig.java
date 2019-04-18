5c320c499c64391e22d434b8cee89c8bbc8db4b3
louwweiwei <louweiwei@sinosoft.com.cn>
2019-04-18 14:47:36 +0800

    数据库添加批量操作配置

--- src/main/java/com/suitupmonkey/system/config/DBConfig.java
+++ src/main/java/com/suitupmonkey/system/config/DBConfig.java
@@ -1,13 +1,17 @@
 package com.suitupmonkey.system.config;
 
+import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 /**
  * database connection
@@ -22,53 +26,35 @@ public class DBConfig {
     private String url;
     @Value("${spring.datasource.driver}")
     private String driver;
+    @Value("${spring.datasource.filters}")
+    private String filters;
 
-
-
-   /* public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Value("${spring.datasource.password}")
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Value("${spring.datasource.url}")
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    @Value("${spring.datasource.driver}")
-    public void setDriver(String driver) {
-        this.driver = driver;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getDriver() {
-        return driver;
-    }*/
 
     @Bean
     public DataSource dataSource(){
-        DataSource dataSource = new DruidDataSource();
+        DruidDataSource dataSource = new DruidDataSource();
 
-        ((DruidDataSource) dataSource).setUsername(username);
-        ((DruidDataSource) dataSource).setPassword(password);
-        ((DruidDataSource) dataSource).setUrl(url);
-        ((DruidDataSource) dataSource).setDriverClassName(driver);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driver);
+
+        try{
+            dataSource.setFilters(filters);
+            //允许批量操作
+            WallConfig wallConfig = new WallConfig();
+            wallConfig.setMultiStatementAllow(true);
+            wallConfig.setNoneBaseStatementAllow(true);
+
+            List<Filter> proxyFilters = dataSource.getProxyFilters();
+            for (Filter filter : proxyFilters) {
+                assert filter instanceof WallFilter;//if it is a wall filter
+                ((WallFilter) filter).setConfig(wallConfig);//覆盖默认的wallconfig
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
         return dataSource;
     }
 
