62ebb0e3c31029e93d296e182a66f0a1b6fa0f70
suitupmonkey <526002240@qq.com>
2020-04-18 12:41:08 +0800

    1、class util 更新正则校验方法

--- src/main/java/com/suitupmonkey/system/config/DBConfig.java
+++ src/main/java/com/suitupmonkey/system/config/DBConfig.java
@@ -2,11 +2,9 @@ package com.suitupmonkey.system.config;
 
 import com.alibaba.druid.filter.Filter;
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.WebStatFilter;
 import com.alibaba.druid.wall.WallConfig;
 import com.alibaba.druid.wall.WallFilter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -33,19 +31,16 @@ public class DBConfig {
     @Bean
     public DataSource dataSource(){
         DruidDataSource dataSource = new DruidDataSource();
-
         dataSource.setUsername(username);
         dataSource.setPassword(password);
         dataSource.setUrl(url);
         dataSource.setDriverClassName(driver);
-
         try{
             dataSource.setFilters(filters);
             //允许批量操作
             WallConfig wallConfig = new WallConfig();
             wallConfig.setMultiStatementAllow(true);
             wallConfig.setNoneBaseStatementAllow(true);
-
             List<Filter> proxyFilters = dataSource.getProxyFilters();
             for (Filter filter : proxyFilters) {
                 if(filter instanceof WallFilter){
@@ -55,7 +50,6 @@ public class DBConfig {
         }catch (Exception e){
             e.printStackTrace();
         }
-
         return dataSource;
     }
 
