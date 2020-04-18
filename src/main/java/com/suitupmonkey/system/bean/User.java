8cb357add0442fb8ef5565c2434c7f84c0403834
suitupmonkey <526002240@qq.com>
2020-04-18 12:42:24 +0800

    1、user类使用lombok

--- src/main/java/com/suitupmonkey/system/bean/User.java
+++ src/main/java/com/suitupmonkey/system/bean/User.java
@@ -1,57 +1,22 @@
 package com.suitupmonkey.system.bean;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Tolerate;
+
 import java.io.Serializable;
 
+@Data
+@Builder
 public class User implements Serializable {
-
-    //主键
+    /**主键*/
     private String id;
-    //用户名
+    /**用户名*/
     private String username;
-    //密码
+    /**密码*/
     private String password;
-    //头像地址
-    private String head;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getHead() {
-        return head;
-    }
-
-    public void setHead(String head) {
-        this.head = head;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id='" + id + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", head='" + head + '\'' +
-                '}';
-    }
+    /**头像地址*/
+    private long head;
+    @Tolerate
+    public User(){}
 }
